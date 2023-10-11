ebaaabfcc4e3120f042a24ca525944c83037addc
MIKU <thy3972@outlook.com>
2023-10-11 12:01:09 +0800

    30

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,14 +5,15 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let a = ???
-
+    let mut a: [&str; 10] = ["01234567"; 10];
+    a[9] = "0123456789";
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
+    println!("{}", a[9])
 }
