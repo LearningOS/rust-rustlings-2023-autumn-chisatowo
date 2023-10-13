cd9767e366bd1475751d6ad80da47e82d13cc5cf
MIKU <thy3972@outlook.com>
2023-10-13 11:33:01 +0800

    83

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +15,16 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let mut result = 1;
+    let mut i = num;
+    loop {
+        if i == 0 {
+            break;
+        }
+        result *= i;
+        i -= 1;
+    }
+    return result;
 }
 
 #[cfg(test)]
